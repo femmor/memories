df8815c20cc886ffe1f859dc01574a1b5f6f1371
Emmanuel Egomson <fegomson@gmail.com>
2021-04-15 18:03:31 -0500

    Creating a Post form Input

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+
 import useStyles from "./styles"
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: ''
+  })
+
   const classes = useStyles()
 
+  const handleSubmit = () => {}
+
   return (
-    <h1>
-      Form
-    </h1>
+    <Paper className={classes.paper}>
+      <Form 
+        autocomplete="off" 
+        noValidate 
+        className={classes.form} 
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          Create a memory
+        </Typography>
+        <TextField 
+          name="creator" 
+          variant="outline" 
+          label="Creator" 
+          fullWidthh
+          value={postData.creator}
+          onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
+        />
+      </Form>
+    </Paper>
   )
 }
 
