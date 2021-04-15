9d9987f1a08c8ceb1b67a8034a0e65e3a1a17b31
Emmanuel Egomson <fegomson@gmail.com>
2021-04-15 17:08:33 -0500

    Setup action and reducer for post payload

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useDispatch} from 'react-redux';
+import { getPosts } from './actions/posts'
+
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core"
 import memories from './images/logo.png'
 import Form from './components/Form/Form';
@@ -7,6 +10,11 @@ import useStyles from "./styles"
 
 function App() {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   return (
     <Container maxWidth="lg">
