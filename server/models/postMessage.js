7cb150c2e7e0121a024dcf8e88dae7847df261b9
Emmanuel Egomson <fegomson@gmail.com>
2021-04-16 10:57:57 -0500

    Refactored the postSchema

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -4,16 +4,12 @@ const postSchema = mongoose.Schema({
   title: String,
   message: String,
   creator: String,
-  tags: String,
+  tags: [String],
   selectedFile: String,
   likeCount: {
     type: Number,
     default: 0,
   },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  }
 }, {
   timestamps: true
 })
